4fe0a21dc2c70422a6594b3de0a33962dab4b44c
Alon Levy <alevy@redhat.com>
2010-11-18 23:36:08 +0200

    test_pygame: add r key (and refactor key callbacks)

--- test_pygame.py
+++ test_pygame.py
@@ -39,48 +39,57 @@ def test_pygame(default_homes = [(2,2), (3,7)]):
     #make_map, make_homes = random_map_pair
     make_map, make_homes = random_homes_pair
     #make_map, make_homes = test_pair
-    zmap, homes = make_map_with_ants_on_vacancies(
-        default_homes=default_homes,
-        make_map=make_map, make_homes=make_homes)
-    board_size = (len(zmap), len(zmap[0]))
-    steps = 1
+    class Data(object):
+        pass
+    s = Data()
+    s.steps = 1
     cont = True
     renderer = PyGameRenderer()
-    num = 0
+    s.num = 0
     action = lambda x, y: x + y
+    def next_step(k):
+        s.steps += 1
+    def prev_step(k):
+        s.steps = max(0, s.steps - 1)
+    def randomize(k):
+        s.zmap, s.homes = make_map_with_ants_on_vacancies(
+            default_homes=default_homes,
+            make_map=make_map, make_homes=make_homes)
+        s.board_size = (len(s.zmap), len(s.zmap[0]))
+    randomize(None)
+    keys = {'a': next_step, 'z': prev_step,
+            'r': randomize}
     while cont:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == KEYDOWN and event.unicode == 'q':
                 cont = False
             elif event.type == KEYDOWN:
-                if event.unicode == 'a':
-                    steps += 1
-                elif event.unicode == 'z':
-                    steps = max(0, steps - 1)
+                if event.unicode in keys.keys():
+                    keys[event.unicode](event.unicode)
                 elif ord('0') <= event.key <= ord('9'):
-                    num = num*10 + event.key - ord('0')
+                    s.num = s.num*10 + event.key - ord('0')
                 elif event.unicode == '-':
                     action = lambda x, y: x - y
                 elif event.key == 13:
-                    steps = max(0, action(steps, num))
-                    num = 0
+                    s.steps = max(0, action(s.steps, s.num))
+                    s.num = 0
                     action = lambda x, y: x + y
                 elif event.unicode == 'p':
                     import pdb; pdb.set_trace()
-            pygame.display.set_caption(str(steps))
+            pygame.display.set_caption(str(s.steps))
         if cont:
-            goa=run.GOARun('', board_size=board_size, ant_locations=homes)
+            goa=run.GOARun('', board_size=s.board_size, ant_locations=s.homes)
             #goa.grid.display()
-            goa.set_map(zmap)
+            goa.set_map(s.zmap)
             #goa.grid.display()
             done = False
-            for i in xrange(steps):
+            for i in xrange(s.steps):
                 done = goa.single_step()
                 if done:
                     done_step = i
                     break
             if done:
-                pygame.display.set_caption('MEETING - %d (%d)' % (done_step, steps))
+                pygame.display.set_caption('MEETING - %d (%d)' % (done_step, s.steps))
             renderer.render(goa.grid, 1)
             pygame.time.delay(10)
 
