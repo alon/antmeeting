f89be0005e624c3e1a71824c7ea9ac16ea2cb8bc
Alon Levy <alevy@redhat.com>
2012-01-23 00:35:31 +0200

    multiprocessing for everything
    saves results if you kill the process, haven't looked at the results
    really, but query retrieves them, so probably intact (enough for
    cPickle), also, ctrl-c doesn't work right, but you don't have to kill
    with 9, regular kill (SIGHUP?) works fine.

--- multiple_runs.py
+++ multiple_runs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pypy
 
 from collections import namedtuple
+from multiprocessing import Pool, cpu_count, Process, active_children
 from glob import glob
 import itertools
 import AStar
@@ -33,13 +34,14 @@ def pos(homes, mapdata, x, y):
    return (str(homes.index((x,y)) + 1)
            if (x,y) in homes else snart[mapdata[(x,y)]])
 
-def astar(homes, zmap):
+def astar(homes, zmap, verbose=False):
     startpoint, endpoint = homes = map(tuple, homes)
     width, height = len(zmap), len(zmap[0])
     trans = {'*':-1, ' ':1}
     mapdata = dict([((x,y),trans[zmap[x][y]]) for x,y in xys(width, height)])
-    for x in range(width):
-        print ''.join(pos(homes,mapdata,x,y) for y in range(height))
+    if verbose:
+        for x in range(width):
+            print ''.join(pos(homes,mapdata,x,y) for y in range(height))
     astar = AStar.AStar(AStar.SQ_MapHandler(mapdata, width, height))
     start = AStar.SQ_Location(startpoint[0],startpoint[1])
     end = AStar.SQ_Location(endpoint[0],endpoint[1])
@@ -97,9 +99,7 @@ def generate_map_homes_data(N, number_of_runs=50):
     movingai_open = [lambda maze=maps.read_movingai(m): maze for m in glob('movingai/open/*.map')]
     p = itertools.product
     c = itertools.chain
-    for maze_gen, extend_map in c(
-            p(movingai_closed, [False]), p(movingai_open, [True]),
-            p(fixed_mazes + fixed_percent, [False, True])):
+    def do_one((maze_gen, extend_map)):
         map_count = 0
         while map_count < number_of_runs:
             thrown = 0
@@ -114,10 +114,56 @@ def generate_map_homes_data(N, number_of_runs=50):
                 xmap = extend_and_add_trap_to_map(zmap)
             else:
                 xmap = zmap
-            yield xmap, homes, {'closed': not extend_map}
+            return xmap, homes, {'closed': not extend_map}
             map_count += 1
             #astar, ROA, ROA one ant, GOA, GOA one ant
             #    time, num of pheromones
+
+    pairs = c(p(movingai_closed, [False]), p(movingai_open, [True]),
+              p(fixed_mazes + fixed_percent, [False, True]))
+    # XXX - can always turn this back to a yield, but then need to move the
+    # consumer into the p.map, into do_one, to gain multiprocessing?
+    ret = pmap_interleaved(pool, do_one, pairs)
+    return ret
+
+def interleave(it, ncpu):
+    l = list(it)
+    indices = [xrange(x, len(l), ncpu) for x in range(ncpu)]
+    reordered = sum([[l[i] for i in single] for single in indices], [])
+    assert(len(reordered) == len(l))
+    return reordered
+
+def undo_interleave(l, ncpu):
+    """ not symmetric with slice - wants a list """
+    ret = [None for i in len(rei)]
+    indices = [xrange(x, len(l), ncpu) for x in range(ncpu)]
+    i_out = 0
+    for single in indices:
+        for i in single:
+            ret[i] = l[i_out]
+            i_out += 1
+    return ret
+
+def pmap_interleaved(pool, do_one, it):
+    """
+    pool.map l with do_one, but instead of splitting linearly split
+    by offsets:
+    instead of [A, A, B, B, C, C, D, D]
+    do         [A, B, C, D, A, B, C, D]
+    Returns the results in the original order
+    """
+    l = list(it)
+    indices = [xrange(x, len(l), 4) for x in range(4)]
+    reordered = sum([[l[i] for i in single] for single in indices], [])
+    assert(len(reordered) == len(l))
+    rei = results_interleaved = pool.map(do_one, l)
+    ret = [None for i in len(rei)]
+    i_out = 0
+    for single in indices:
+        for i in single:
+            ret[i] = rei[i_out]
+            i_out += 1
+    return ret
 
 class Data(object):
     pass
@@ -181,10 +227,11 @@ def results_filename(N):
     return os.path.join(str(N), 'ant_results.sqlite3')
 
 def ctrl_c_handler(*args):
-    print "Ctrl-C caught, exiting"
+    print "Ctrl-C caught (%d), exiting" % os.getpid()
+    for c in active_children():
+        print "terminating %s" % c
+        c.terminate()
     sys.exit(1)
-
-signal.signal(signal.SIGINT, ctrl_c_handler)
 
 def do_multiple(N):
     if not N:
@@ -194,7 +241,7 @@ def do_multiple(N):
     print "loading map file %s" % filename
     with open(filename, 'r') as f:
         map_home_data = cPickle.load(f)
-    print "have to calculate %d datum" % len(map_home_data)
+    print "total %d datum required" % len(map_home_data)
     dbfile = results_filename(N)
     print "opening databsase %s" % dbfile
     con = sqlite3.connect(dbfile)
@@ -203,13 +250,26 @@ def do_multiple(N):
         con.execute('create table results (params, result)')
     except sqlite3.OperationalError:
         pass
+    inputs = []
     for i, (the_map, homes, closed) in enumerate(map_home_data):
         params = make_params(the_map, homes)
         if con.execute('select count(*) from results where params=?', [params]).fetchall()[0][0] > 0:
-            print "%d/%d already calculated" % (i, len(map_home_data))
             continue
-        print "calculating %d/%d" % (i, len(map_home_data))
+        inputs.append((i, the_map, homes))
+    results_missing = len(inputs)
+    def do_single((i, the_map, homes)):
+        params = make_params(the_map, homes)
+        print "calculating %d/%d" % (i, results_missing)
         results = single_run(the_map, homes)
+        return i, params, results
+    print "missing %d datum" % results_missing
+    print "running parallel on %d cpus" % cpu_count()
+    p = Pool(cpu_count())
+    sliced_inputs = interleave(inputs, cpu_count())
+    calculated = 0
+    for i, params, results in pool.imap_unordered(do_single, sliced_inputs):
+        calculated += 1
+        print "got results for %s (%d/%d)" % (i, calculated, results_missing)
         con.execute('insert into results values (?, ?)', [params, cPickle.dumps(results)])
         con.commit()
 
@@ -238,5 +298,7 @@ def main():
     # create report
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, ctrl_c_handler)
+    pool = Pool(cpu_count())
     main()
     #map_tester()
